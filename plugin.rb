70e7f74970e4aee445c4bb98fb95623b42c35481
scossar <scossar3@gmail.com>
2016-06-20 17:13:13 -0700

    connect to custom template

--- plugin.rb
+++ plugin.rb
@@ -11,14 +11,43 @@ after_initialize do
       url = @url.sub(/[&?]source_topic_id=(\d+)/, "")
       source_topic_id = $1.to_i
 
-      if route[:controller] == 'topics'
-        if (route[:post_number].present? && route[:post_number].to_i == 1) || route[:post_number].blank?
-          topic = Topic.find(route[:topid_id])
-          return '<h1>this is a dictionary</h1>'
+      if route[:controller] == 'topics' &&
+          ((route[:post_number].present? && route[:post_number].to_i == 1) ||
+              (route[:post_number].blank?))
+        topic = Topic.where(id: route[:topic_id].to_i).includes(:user).first
+        category_slug = topic.category.slug
+
+        if category_slug == 'dictionary'
+          post = topic.posts.first
+          posters = topic.posters_summary.map do |p|
+            {
+                username: p[:user].username,
+                avatar: PrettyText.avatar_img(p[:user].avatar_template, 'tiny'),
+                description: p[:description],
+                extras: p[:extras]
+            }
+          end
+          content = post.raw
+          args = { original_url: url,
+                   title: PrettyText.unescape_emoji(CGI::escapeHTML(topic.title)),
+                   avatar: PrettyText.avatar_img(topic.user.avatar_template, 'tiny'),
+                   posts_count: topic.posts_count,
+                   last_post: FreedomPatches::Rails4.time_ago_in_words(topic.last_posted_at, false, scope: :'datetime.distance_in_words_verbose'),
+                   age: FreedomPatches::Rails4.time_ago_in_words(topic.created_at, false, scope: :'datetime.distance_in_words_verbose'),
+                   views: topic.views,
+                   posters: posters,
+                   content: content,
+                   category_html: CategoryBadge.html_for(topic.category),
+                   topic: topic.id }
+
+          return Mustache.render(File.read("#{Rails.root}/plugins/dictionary-card-onebox/lib/onebox/templates/dictionary_card_onebox.hbs"), args)
         else
           super_to_html
         end
+      else
+        super_to_html
       end
+      super_to_html
     end
   end
 end
